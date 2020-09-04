56375b8abc2784ba6f06ba551a58cbc7a5e0ea2c
YinPeng <myyinpeng@qq.com>
2020-09-04 17:14:10 +0800

    Updates

---
+++ precache-manifest.645ec859027caaa6bc50ece2cdd4fd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577e439305447d872019d108c3b9ea2b",
+    "url": "/react-Y/index.html"
+  },
+  {
+    "revision": "3b52675132b8b6059ff6",
+    "url": "/react-Y/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/react-Y/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Y/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b52675132b8b6059ff6",
+    "url": "/react-Y/static/js/main.f34eb2f8.chunk.js"
+  },
+  {
+    "revision": "eead2a6b29b9457a1fee",
+    "url": "/react-Y/static/js/runtime-main.88e7eb7d.js"
+  }
+]);
